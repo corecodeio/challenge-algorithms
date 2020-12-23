380170633f2df3c5099d28f72bfb4728ffdf3da5
Sebastian Ibarguen <sebasibarguen@users.noreply.github.com>
2020-12-23 14:16:28 -0600

    Add FizzBuzz solution

--- challenges/fizzbuzz/solution.js
+++ challenges/fizzbuzz/solution.js
@@ -1,7 +1,16 @@
 function FizzBuzz(num)
 {
-    
-    // code goes here  
-    return num;
+    let result = [];
+    for(let i = 1; i <= num; i++){
+        if (i % 15 == 0)
+            result.push("FizzBuzz");
+        else if (i % 3 == 0)
+            result.push("Fizz");
+        else if (i % 5 == 0)
+            result.push("Buzz");
+        else
+            result.push(i.toString());
+    }
+    return result.join(" ");
 }
 module.exports = FizzBuzz;
