482f316e5c94441af6a7919327bcd7c0133b8fda
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2021-01-08 18:17:17 -0600

    Word split done

---
+++ challenges/word_split/pugaIsaias/solution.test.js
@@ -0,0 +1,13 @@
+const WordSplit = require("./solution");
+
+test('WordSplit(["baseball", "a,all,b,ball,bas,base,cat,code,d,e,quit,z"])', () => {
+  expect(
+    WordSplit(["baseball", "a,all,b,ball,bas,base,cat,code,d,e,quit,z"])
+  ).toBe("base,ball");
+});
+
+test('WordSplit(["abcgefd", "a,ab,abc,abcg,b,c,dog,e,efd,zzzz"])', () => {
+  expect(WordSplit(["abcgefd", "a,ab,abc,abcg,b,c,dog,e,efd,zzzz"])).toBe(
+    "abcg,efd"
+  );
+});
