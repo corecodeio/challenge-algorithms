48f1c9b54b644103e2bbf1313eac7437ea1b6845
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2021-01-08 18:24:42 -0600

    Recommended fix to initial test for FizzBuzz(8)

--- challenges/fizzbuzz/_test/solution.test.js
+++ challenges/fizzbuzz/_test/solution.test.js
@@ -1,10 +1,9 @@
-const FizzBuzz = require('../solution');
+const FizzBuzz = require("../solution");
 
-
-test('FizzBuzz(3)', ()=> {
-    expect(FizzBuzz(3)).toBe('1 2 Fizz');
+test("FizzBuzz(3)", () => {
+  expect(FizzBuzz(3)).toBe("1 2 Fizz");
 });
 
-test('FizzBuzz(8)', ()=> {
-    expect(FizzBuzz(3)).toBe('1 2 Fizz 4 Buzz Fizz 7 8');
+test("FizzBuzz(8)", () => {
+  expect(FizzBuzz(8)).toBe("1 2 Fizz 4 Buzz Fizz 7 8");
 });
